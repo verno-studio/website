664d98f54857853c0aa5dc8d81dd1ee17c2bfe25
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-04-26 22:06:14 +0700

    feat(web): enhance metadata and add robots and sitemap generation

---
+++ apps/web/app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+import { url } from "@/lib/url";
+
+const robots = (): MetadataRoute.Robots => ({
+  rules: [
+    {
+      allow: "/",
+      userAgent: "*",
+    },
+  ],
+  sitemap: new URL("/sitemap.xml", url).href,
+});
+
+export default robots;
